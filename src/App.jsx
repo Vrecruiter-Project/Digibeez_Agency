f550f1e6dd4f917dc36306923a0cd7c161c1034b
Ayush Kumar <ayush.dhiman0610@gmail.com>
2025-02-05 16:23:39 +0530

    add video background and call-to-action buttons to Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Nav from './components/Nav/Nav'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <>
-
+      <Nav />
+      <Home />
+      <Footer />
     </>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../components/Herosection/hero'
 
 export default function Home() {
   return (
     <>
-    
+      <Hero />
+
     </>
   )
 }
